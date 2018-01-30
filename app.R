81f2c749e0b39f8eef1bbb0d3790a52e0ff21e63
Alex Lishinski <alexlishinski@gmail.com>
2018-01-30 17:47:47 -0500

    Changed it to reflect probability of birth happening on current day

--- app.R
+++ app.R
@@ -11,13 +11,13 @@ library(shiny)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
-    
+
     shinythemes::shinytheme("cerulean"),
-    
+
     # Application title
     titlePanel("Due distribution calculator"),
-    
-    # Sidebar with a slider input for number of bins 
+
+    # Sidebar with a slider input for number of bins
     sidebarLayout(
         sidebarPanel(
             dateInput("date",
@@ -26,7 +26,7 @@ ui <- fluidPage(
                          "Enter the standard deviation for the due date (defaults to 9.5):",
                          9.5)
         ),
-        
+
         # Show a plot of the generated distribution
         mainPanel(
             plotOutput("distPlot"),
@@ -36,45 +36,61 @@ ui <- fluidPage(
 )
 
 create_plot <- function(due_date, my_sd) {
-    
+
     # lower_bound <- qnorm(.025) * my_sd
     # upper_bound <- qnorm(.975) * my_sd
-    
-    date_today <- Sys.Date()
-    due_date <- anytime::anydate(due_date)
-    diff <- date_today - due_date
-    
-    pr <- pnorm(diff / my_sd)
-    
+
+  due_dist <- read.csv("preg due.csv", stringsAsFactors = F)
+  due_dist$Progress <- str_c(str_trim(due_dist$Progress), str_trim(due_dist$p2), sep = "")
+
+  cprob <- function(x, y, xy){
+    (xy * y) / x
+  }
+
+  pcts <- as.numeric(str_sub(due_dist$On.this.date.overall, 1, -3)) * .01
+  pcts_cum <- as.numeric(str_sub(due_dist$By.this.date.overall, 1, -3)) * .01
+  due_dist$pcts_day <- mapply(FUN = cprob, x = 1 - pcts_cum, y = pcts, xy = 1.00)
+
+  library(lubridate)
+  due_date <- anytime::anydate(due_date)
+  date_today <- Sys.Date()
+  diff <- date_today - due_date
+  n_days <- days(diff)
+
+  #pr <- pnorm(n_days@day / my_sd)
+  pr <- round(due_dist[which(due_dist$Progress == "40W0D") + n_days@day, "pcts_day"], digits = 3)
+
+  data = data.frame(x = c(-35, 35))
+
     library(ggplot2)
-    
-    p <- ggplot(aes(x), data = data.frame(x = c(-35, 35))) +
-        stat_function(fun = dnorm, n = 1000, args = list(mean = 0, sd = my_sd)) +
-        scale_x_continuous() +
-        xlab("Days before or after due date") +
-        geom_vline(xintercept = diff, color = "red") +
-        geom_text(aes(x = diff, 
-                      label = paste0("Pr(Baby born by today's date) = ", 
-                                     round(pr, 3)), 
-                      y = .005, 
-                      color = "red",
-                      size = 18)) +
-        theme_bw() +
-        theme(legend.position = "none", text = element_text(size = 16)) +
-        ylab("Pr(Baby born)")
-    
+
+    p <- ggplot(data, aes(x)) +
+      stat_function(fun = dnorm, n = 1000, args = list(mean = 0, sd = my_sd)) +
+      scale_x_continuous() +
+      xlab("Days before or after due date") +
+      geom_vline(xintercept = diff, color = "red") +
+      geom_text(aes(label = paste0("Pr(Baby born on today's date) = ",
+                                   round(pr, 3)),
+                    y = .005,
+                    x = 1,
+                    color = "red",
+                    size = 24)) +
+      theme_bw() +
+      theme(legend.position = "none", text = element_text(size = 16)) +
+      ylab("Pr(Baby born)")
+
     p
-    
+
 }
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-    
+
     output$distPlot <- renderPlot({
         create_plot(input$date, input$my_sd)
     })
 }
 
-# Run the application 
+# Run the application
 shinyApp(ui = ui, server = server)
 
